a252f1a0622e5c42741521fb8a62c362608a3dce
Zargham Khan <zargham.khan@leanix.net>
2024-12-05 23:40:59 +0100

    Reproduce TypeError

--- libs/repro/src/lib/repro/repro.component.spec.ts
+++ libs/repro/src/lib/repro/repro.component.spec.ts
@@ -1,11 +1,22 @@
-import { fakeAsync, tick } from '@angular/core/testing';
+import { ChangeDetectorRef } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import {
+  TranslateModule,
+  TranslatePipe,
+  TranslateService,
+} from '@ngx-translate/core';
 import { render } from '@testing-library/angular';
 import { ReproComponent } from './repro.component';
 
 describe('ReproComponent', () => {
-  it('should create', fakeAsync(async () => {
-    const component = await render(ReproComponent, {});
-    tick(1000);
-    expect(component).toBeTruthy();
-  }));
+  it('should create', async () => {
+    await render(ReproComponent, {
+      imports: [TranslateModule.forRoot()],
+      providers: [{ provide: ChangeDetectorRef, useValue: {} }],
+    });
+    const service = TestBed.inject(TranslateService);
+    const cd = TestBed.inject(ChangeDetectorRef);
+    const pipe = new TranslatePipe(service, cd);
+    expect(pipe.transform('test')).toBeTruthy();
+  });
 });
